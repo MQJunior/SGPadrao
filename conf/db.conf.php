eafe2fdd560cec462adbff290be0f6026794c20a
MQJunior <77548865+MQJunior@users.noreply.github.com>
2024-03-08 22:56:55 -0300

    Ajustes simples

--- conf/db.conf.php
+++ conf/db.conf.php
@@ -1,65 +1,24 @@
 <?php
 /**
-* db.sgpadrao.conf.php
-*
-* SGPadrao - Configuração DB
-*
-* Definições do Banco de dados
-*
-* @date   2018-01-10
-*
-* @author       Marcio Queiroz Jr <mqjunior@gmail.com>
-* @version      1.0
-* @copyright    Copyright © 2006, Marcio Queiroz Jr.
-* @package      SGPadrao
-* @subpackage   Config
-* @category     DataBase
-*/
-
-/**
- * Define o NOME do Sistema;
- * @global  string    $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['NOME']
- * @name              $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['NOME']
+ * db.sgpadrao.conf.php
+ *
+ * SGPadrao - Configuração DB
+ *
+ * Definições do Banco de dados
+ *
+ * @date   2018-01-10
+ *
+ * @author       Marcio Queiroz Jr <mqjunior@gmail.com>
+ * @version      1.0
+ * @copyright    Copyright © 2006, Marcio Queiroz Jr.
+ * @package      SGPadrao
+ * @subpackage   Config
+ * @category     DataBase
  */
-$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['NOME'] = 'sgpadrao';
 
-/**
- * Define o ENDERECO do servidor DB;
- * @global  string    $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['HOSTNAME']
- * @name              $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['HOSTNAME']
- */
-$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['HOSTNAME'] = 'localhost';
-
-/**
- * Define o nome do USUARIO no Banco de Dados;
- * @global  string    $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['USERNAME']
- * @name              $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['USERNAME']
- */
-//$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['USERNAME'] = 'jahisaco_z2sys'; // WEB
-$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['USERNAME'] = 'SGPadrao'; 	// LOCAL
-
-/**
- * Define a SENHA de acesso ao Banco de Dados;
- * @global  string    $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['PASSWORD']
- * @name              $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['PASSWORD']
- */
-//$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['PASSWORD'] = 'qaz951357';   // WEB
-$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['PASSWORD'] = 'T1CuXi8I'; 	// LOCAL
-
-/**
- * Define o nome da BASE de dados;
- * @global  string    $SISTEMA['DATABASE']['DATABASENAME']
- * @name              $SISTEMA['DATABASE']['DATABASENAME']
- */
-//$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['DATABASENAME'] = 'jahisaco_z2system'; 	// WEB
-$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['DATABASENAME'] = 'sgpadrao';				// LOCAL	
-
-/**
- * Define o tipo de Banco de Dados
- * {0 - MySQL|1-Firebird} ;
- * @global  string    $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['TIPODB']
- * @name              $SISTEMA['CONFIG']['SISTEMA']['DATABASE']['TIPODB']
- */
-$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['TIPODB'] = '0';
-
-?>
+$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['NOME'] = 'sgpadrao'; // Define o NOME da Base de Dados
+$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['HOSTNAME'] = 'localhost'; // Define o ENDEREÇO do servidor DB
+$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['USERNAME'] = 'SGPadrao'; // username
+$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['PASSWORD'] = 'T1CuXi8I'; // Senha
+$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['DATABASENAME'] = 'sgpadrao'; // Data Base Name
+$SISTEMA['CONFIG']['SISTEMA']['DATABASE']['TIPODB'] = '0'; // Define o tipo de Banco de Dados {0 - MySQL|1-Firebird}
